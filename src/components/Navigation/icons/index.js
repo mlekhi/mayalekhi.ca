d2d9682c3a07e51cde8249f75d345cd9a4446e15
mlekhi <maya.lekhi1@gmail.com>
2024-01-10 17:07:26 -0500

    fixing taio's changes

--- src/components/Navigation/icons/index.js
+++ src/components/Navigation/icons/index.js
@@ -1,6 +1,3 @@
-export { ReactComponent as Twitter } from "./twitter-icon.svg";
 export { ReactComponent as LinkedIn } from "./linkedin-icon.svg";
 export { ReactComponent as GitHub } from "./github-icon.svg";
 export { ReactComponent as Email } from "./email-icon.svg";
-export { ReactComponent as CV } from "./cv-icon.svg";
-export { ReactComponent as Room } from "./room-icon.svg";
